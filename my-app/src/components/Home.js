4c3b2385b8da05cfcd8b55e82c9f5f8ea96225d1
wys0620 <752096519@qq.com>
2017-09-15 17:28:56 +0800

    这是首页刚开始

--- my-app/src/components/Home.js
+++ my-app/src/components/Home.js
@@ -3,7 +3,21 @@ import React from 'react'
 export default class Detail extends React.Component {
 	render() {
 		return (
-			<div>home</div>
+			<div>
+				<header>
+					<div className="head_bar">
+						<a href="javascript:;" className="all">全部</a>
+						<a href="javascript:;" className="search">搜索</a>
+						<a href="javascript:;" className="cart">购物车</a>
+						<a href="javascript:;" className="my">消息</a>
+					</div>
+					<ul className="nav">
+						<li><a href="">男装</a></li>
+						<li><a href="">女装</a></li>
+						<li><a href="">美妆</a></li>
+					</ul>
+				</header>
+			</div>
 		)
 	}
 }
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import './iconfont/iconfont.css'
+import './iconfont/iconfont.css';
+import './css/home.css';
+
+
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
