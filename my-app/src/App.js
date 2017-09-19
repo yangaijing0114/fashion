191dbf42d9d67bd843ede3c12ef4d9eb89b07072
yangaijing0114 <1223382034@qq.com>
2017-09-19 20:47:10 +0800

    mine的第三次提交

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,11 +8,6 @@ import {
 } from 'react-router-dom'
 import { DatePicker, message } from 'antd';
 
-<<<<<<< HEAD
-=======
-import { DatePicker, message} from 'antd';
->>>>>>> 4f440dbb4231f4240db89d5eece2b43a4b26307e
-
 import Home from './components/Home'
 import Classify from './components/Classify'
 import Shopcart from './components/Shopcart'
@@ -30,7 +25,7 @@ const App = () => (
         <li><i className="icon iconfont icon-shouye"></i><Link to="/">首页</Link></li>
         <li><i className="icon iconfont icon-fenlei"></i><Link to="/classify">分类</Link></li>
         <li><i className="icon iconfont icon-gouwuche"></i><Link to="/shopcart">购物车</Link></li>
-        <li><i className="icon iconfont icon-wode"></i><Link to="/mine">我的</Link></li>
+        <li><i className="icon iconfont icon-wode"></i><a href="/mine">我的</a></li>
       </ul>      
     </div>
   </Router>
