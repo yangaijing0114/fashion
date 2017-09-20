6eb98daf625de893bf5bef9df62c2dcdd34b8434
月色 <1223382034@qq.com>
2017-09-20 12:03:03 +0800

    提交更新测试

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,22 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-<<<<<<< HEAD
-=======
+
 
 import './iconfont/iconfont.css'
 
-
-
->>>>>>> 010b256bfc5c05610f728960098f8194450db9f6
-
-import './iconfont/iconfont.css'
-
-<<<<<<< HEAD
-=======
-import './iconfont/iconfont.css';
-
->>>>>>> 010b256bfc5c05610f728960098f8194450db9f6
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
